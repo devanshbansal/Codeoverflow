1f93138d17a3ccdea85651374b958322c63fad04
ma5terdrag0n <prince.thakkar001@gmail.com>
2017-09-28 09:01:07 -0700

    adding files

---
+++ app/src/main/java/com/example/hp/codeoverflow2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hp.codeoverflow2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Interpolator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void login(View v){
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+    public void signup(View v){
+        Intent intent = new Intent(this, signup.class);
+        startActivity(intent);
+    }
+}
